33d794e91b4683da67ce96855de31fd0bac91e93
Documenter.jl <documenter@juliadocs.github.io>
2024-10-30 08:49:04 +0000

    build based on 609ef6b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Wahoo","category":"page"},{"location":"#Wahoo","page":"Home","title":"Wahoo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Wahoo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Wahoo]","category":"page"}]
+}
