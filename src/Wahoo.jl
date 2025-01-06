464b4d7ac6492e3cc2e3ce43f4fcb8a31371520f
scheidan <andreas.scheidegger@eawag.ch>
2025-01-06 15:04:13 +0100

    first draft for smoothing

--- src/Wahoo.jl
+++ src/Wahoo.jl
@@ -1,6 +1,6 @@
 module Wahoo
 
-export forward_filter
+export track
 
 using CUDA
 import NNlib
@@ -18,8 +18,8 @@ h: spatial resolution
 function make_kernel(;D, h)
 
     # compute 1/Δ, so that 4*D*Δ/h^2 < 1
-    n_steps = ceil(Int, 4*D/(h^2*0.99))
-    Δ = 1f0/n_steps
+    n_hops = ceil(Int, 4*D/(h^2*0.99))
+    Δ = 1f0/n_hops
     @assert 4*D*Δ/h^2 < 1
 
     # convolution kernel
@@ -32,7 +32,7 @@ function make_kernel(;D, h)
                                         0  1  0]
     H = reshape(H, 3, 3, 1, 1)
 
-    H, n_steps
+    H, n_hops
 end
 
 
@@ -86,13 +86,14 @@ function build_distances(coord::Vector,
     dist_acoustic
 end
 
+
 # ---
-# run filter
+# filter algorithm
 
 function run_filter!(pos, H,
                      bathymetry, depth_obs,
                      acoustic_obs, dist_acoustic;
-                     steps_per_t=1,
+                     hops_per_step,
                      tsave=1:100,
                      p_init)
 
@@ -100,16 +101,16 @@ function run_filter!(pos, H,
 
     nx, ny = size(pos)[1:2]
     pos_tmp = similar(pos, nx, ny, 1, 1)
-    pos_tmp[:,:,1,1] = p_init ./ sum(p_init)
+    pos_tmp[:,:,1,1] .= p_init ./ sum(p_init)
+    pos[:,:,1,1] .= pos_tmp[:,:,1,1]
 
-    i = 1
-    for t in 1:tmax
+    for t in 2:tmax
 
         # --- solve focker plank
-        for k in 1:steps_per_t
-            CUDA.@allowscalar begin
+        for k in 1:hops_per_step
+            #CUDA.@allowscalar begin
                 pos_tmp[:,:,1,1] = NNlib.conv(pos_tmp[:,:,1:1,1:1], H, pad=1)
-            end
+            #end
         end
 
         # --- add depth signal
@@ -128,17 +129,142 @@ function run_filter!(pos, H,
         pos_tmp[:,:,1,1] .= pos_tmp[:,:,1,1] ./ Z
 
         # --- save results
+
         if t in tsave
-            pos[:,:,1,i] = pos_tmp[:,:,1,1]
-            i += 1
+            pos[:,:,1,time2index(t, tsave)] = pos_tmp[:,:,1,1]
         end
     end
 
 end
 
 
+# ---
+# smoothing algorithm
+
+
 """
-Performs forward filtering using a diffusion model
+Define division by zero equal to zero.
+"""
+divzero(a, b) = iszero(b) ? zero(a/b) : a/b
+
+
+"""
+Maps time step to the index of `pos_filter` and `pos_smoother`
+"""
+time2index(t, tsave) = findfirst(==(t), tsave)
+
+
+function run_smoother(pos_filter, H,
+                      bathymetry, depth_obs,
+                      acoustic_obs, dist_acoustic;
+                      hops_per_step,
+                      tsave=1:100)
+
+
+    tmax = maximum(tsave)
+    n_tsave = length(tsave)
+
+    nx, ny = size(pos_filter)[1:2]
+
+    # hold smoothed results, P(s_t | y_{1:T})
+    pos_smoother = similar(pos_filter)
+    pos_smoother_tmp = copy(similar(pos_filter, nx, ny, 1:1))
+
+    pos_smoother_tmp[:,:,1] .= pos_smoother[:,:,1,end] .= pos_filter[:,:,1,end]
+
+    # jth jump back in time
+    for j in (n_tsave-1):-1:1
+
+        tsave_jump = tsave[j]:1:tsave[j+1]    # "internal", reconstructed time-steps
+
+        # -----------
+        # 1) recompute all filter steps between j and j+1
+
+        # holds reconstructed filter results (!!! better move out of loop?)
+        pos_filter_jump = similar(pos_filter, nx, ny, 1, length(tsave_jump))         # P(s_t | y_{1:t})
+        pos_filter_jump_moved = similar(pos_filter, nx, ny, 1, length(tsave_jump))   # P(s_{t+1} | y_{1:t})
+
+        pos_filter_jump[:,:,1,1] .= pos_filter_jump_moved[:,:,1,1] .= pos_filter[:,:,1,time2index(tsave_jump[1], tsave)]
+
+
+        for (i,t) in enumerate(tsave_jump[1:(end-1)])
+
+            pos_filter_jump[:,:,1,i+1] .= pos_filter_jump[:,:,1,i]
+
+            # --- solve focker plank
+            for k in 1:hops_per_step
+                CUDA.@allowscalar begin
+                    pos_filter_jump[:,:,1,(i+1):(i+1)] = NNlib.conv(pos_filter_jump[:,:,1:1,(i+1):(i+1)], H, pad=1)
+                end
+            end
+
+            # --- save P(s_{t+1} | y_{1:t})
+            pos_filter_jump_moved[:,:,1,i+1] .= pos_filter_jump[:,:,1,i+1]
+
+            # --- add depth signal
+            pos_filter_jump[:,:,1,i+1] .*= p_depth.(depth_obs[t+1], bathymetry)
+
+            # --- add accustic signal
+            n_sensors = size(dist_acoustic, 3)
+            for s in 1:n_sensors
+                pos_filter_jump[:,:,1,i+1] .*= p_acoustic.(acoustic_obs[s, t+1], dist_acoustic[:,:,s])
+            end
+
+            # --- normalize
+            Z = sum(pos_filter_jump[:,:,1,i+1])
+            isfinite(Z) ||
+                error("No solution at time point $(t)!")
+            pos_filter_jump[:,:,1,i+1] .= pos_filter_jump[:,:,1,i+1] ./ Z
+
+
+        end
+
+        # -----------
+        # 2) backward smoothing
+
+        pos_smoother_tmp[:,:,1,1] .= pos_smoother[:,:,1,time2index(tsave_jump[end], tsave)]
+
+        for (i, t) in enumerate(reverse(tsave_jump)[2:end])
+            idx = length(tsave_jump) - i + 1 # index of pos_filter_jump
+
+            # treat division by zero as special case
+            pos_smoother_tmp[:,:,1,1] .= divzero.(pos_smoother_tmp[:,:,1,1], pos_filter_jump[:,:,1,idx])
+
+            # --- solve focker plank backwards
+            # K = rot180(H) = H if no advections
+            for k in 1:hops_per_step
+                CUDA.@allowscalar begin
+                    pos_smoother_tmp[:,:,1,1] = NNlib.conv(pos_smoother_tmp[:,:,1:1,1:1], H, pad=1)
+                end
+            end
+
+            pos_smoother_tmp[:,:,1,1] .=  pos_filter_jump[:,:,1,idx-1] .* pos_smoother_tmp[:,:,1,1]
+
+            # !!! CHECK why is this normalisation needed / allowed? !!!
+            pos_smoother_tmp[:,:,1,1] ./= sum(pos_smoother_tmp[:,:,1,1])
+
+            # --- save
+            if t in tsave
+                pos_smoother[:,:,1,time2index(t, tsave)] = pos_smoother_tmp[:,:,1,1]
+            end
+
+        end
+
+    end
+
+    pos_smoother, tsave
+end
+
+
+
+"""
+Tracks the location of the fish
+
+```
+track(p_init, bathymetry; depth_obs, acoustic_obs, acoustic_pos, tsave, D, h, smoothing, use_gpu)
+```
+
+Uses forward filtering based on a diffusion model and optionally smoothing.
 
 # Arguments
 
@@ -150,14 +276,17 @@ Performs forward filtering using a diffusion model
 - `tsave::AbstractVector`: Time steps at which to save the probability map
 - `D`: Diffusion coefficient, i.e. variance for one time step movement [m^2]
 - `h`: spatial resolution [m]
+- `smoothing`: Boolean flag to enable smoothing
 - `use_gpu`: Boolean flag to enable GPU
 
 """
-function forward_filter(p_init, bathymetry;
-                        depth_obs,
-                        acoustic_obs, acoustic_pos,
-                        tsave::AbstractVector = 1:100,
-                        D, h=1, use_gpu=false)
+function track(p_init, bathymetry;
+               depth_obs,
+               acoustic_obs, acoustic_pos,
+               tsave::AbstractVector = 1:100,
+               D, h=1,
+               smoothing::Bool=false,
+               use_gpu::Bool=false)
 
     @assert size(p_init) == size(bathymetry)[1:2]
     @assert size(acoustic_pos, 1) == length(acoustic_pos)
@@ -168,9 +297,7 @@ function forward_filter(p_init, bathymetry;
 
 
     # convolution kerel
-    H, n_steps = make_kernel(D=D, h=h)
-
-    println("$((maximum(tsave)-1)*n_steps) convolutions")
+    H, n_hops = make_kernel(D=D, h=h)
 
     # distances to acoustic sensors
     dist_acoustic = build_distances(acoustic_pos, bathymetry, h)
@@ -183,7 +310,7 @@ function forward_filter(p_init, bathymetry;
 
         memory_demand = round(nx * ny * length(tsave) * 4 / 1024^2, digits=0) # in Mega bytes
         memory_free = round(CUDA.free_memory() / 1024^2, digits=0)
-        println("Computation requires at least $memory_demand Mb of GPU memmory ($memory_free Mb free)")
+        println("Filter computation requires at least $memory_demand Mb of GPU memmory ($memory_free Mb free)")
 
         pos = CuArray{Float32}(undef, (nx, ny, 1, length(tsave)))
     else
@@ -191,12 +318,31 @@ function forward_filter(p_init, bathymetry;
         bathymetry = bathymetry[:,:,1]
     end
 
+    println("--- Run filter ---")
+    println(" Requires $((maximum(tsave)-1)*n_hops) convolutions")
+
     run_filter!(pos, H, bathymetry, depth_obs,
                 acoustic_obs, dist_acoustic;
-                steps_per_t=n_steps, tsave=tsave, p_init)
+                hops_per_step = n_hops, tsave = tsave, p_init)
 
-    return (pos=pos, tsave=tsave)
+    if smoothing
+        println("--- Run smoothing ---")
+
+        pos_smoother, tsave = run_smoother(pos, H,
+                                           bathymetry, depth_obs,
+                                           acoustic_obs,
+                                           dist_acoustic;
+                                           hops_per_step = n_hops,
+                                           tsave = tsave)
+
+        return (pos_smoother = pos_smoother, pos_filter = pos, tsave = tsave)
+
+    else
+        return (pos_filter = pos, tsave = tsave)
+    end
+
 end
+
 
 
 end
