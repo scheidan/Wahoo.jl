2db88906cef3e455523341bc4b2e780d8b85e3c8
scheidan <andreas.scheidegger@eawag.ch>
2025-01-08 09:41:49 +0100

    fix extension

--- ext/CUDAExt.jl
+++ ext/CUDAExt.jl
@@ -9,8 +9,11 @@ module CUDAExt
 using Wahoo
 using CUDA
 import cuDNN
+import NNlib
 
 function move_to_GPU(H, bathymetry, dist_acoustic, tsave)
+
+    nx, ny = size(bathymetry)[1:2]
     H = CuArray{Float32}(H)
     bathymetry = CuArray{Float32}(bathymetry[:,:,1])
     dist_acoustic = CuArray{Float32}(dist_acoustic)
@@ -18,6 +21,7 @@ function move_to_GPU(H, bathymetry, dist_acoustic, tsave)
 
     memory_demand = round(nx * ny * length(tsave) * 4 / 1024^2, digits=0) # in Mega bytes
     memory_free = round(CUDA.free_memory() / 1024^2, digits=0)
+	@info "Using GPU: $(CUDA.current_device())"
     @info "Filter computation requires at least $memory_demand Mb of GPU memmory ($memory_free Mb free)"
 
     return H, bathymetry, dist_acoustic, pos
