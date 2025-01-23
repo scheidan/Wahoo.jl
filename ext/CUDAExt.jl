9c1198b8315b8f15631ce00b6f106d3e0145935e
scheidan <andreas.scheidegger@eawag.ch>
2025-01-23 14:10:47 +0100

    remove confusing memory reporting

--- ext/CUDAExt.jl
+++ ext/CUDAExt.jl
@@ -18,11 +18,7 @@ function move_to_GPU(H, bathymetry, dist_acoustic, tsave)
     dist_acoustic = CuArray{Float32}(dist_acoustic)
     pos = CuArray{Float32}(undef, (nx, ny, 1, length(tsave)))
 
-    memory_demand = round(nx * ny * length(tsave) * 4 / 1024^2, digits=0) # in Mega bytes
-    memory_free = round(CUDA.free_memory() / 1024^2, digits=0)
-	@info "Using GPU: $(CUDA.current_device())"
-    @info "Filter computation requires at least $memory_demand Mb of GPU memmory ($memory_free Mb free)"
-
+    @info "Using GPU: $(CUDA.current_device())"
     return H, bathymetry, dist_acoustic, pos
 end
 
